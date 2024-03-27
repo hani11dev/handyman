1fe87227b5406c47568b31d864fef222459dae38
hani <hani11dev@gmail.com>
2024-03-27 17:59:11 +0100

    my requests

--- app/src/main/java/com/example/handyapp/home/myRequests/Request.kt
+++ app/src/main/java/com/example/handyapp/home/myRequests/Request.kt
@@ -1,11 +1,10 @@
 package com.example.handyapp.home.myRequests
 
-
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -16,97 +15,89 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.handyapp.R
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.navigation.NavController
+
+
 data class Request(
     val name: String,
     val service: String,
     val price: Int,
     val currency: String,
     val date: String,
-    var profilePicture: Int? = null // Int? to hold the resource ID of the profile picture
+    val location : String
 )
+
+
 
 @Composable
 fun RequestItem(request: Request) {
+    val backgroundColor = MaterialTheme.colorScheme.surface
+
     Surface(
-        color = MaterialTheme.colorScheme.background,
-        //color = Color.White, // Set background color to white
-        shadowElevation = 10.dp, // Add elevation for shadow effect
+        color = backgroundColor,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 16.dp, horizontal = 16.dp)
+            .padding(vertical = 16.dp, horizontal = 16.dp),
+        shadowElevation = 10.dp,
+        tonalElevation = 5.dp,
+        border = BorderStroke(2.dp, color = Color.Gray)
     ) {
         Card(
             shape = RoundedCornerShape(8.dp),
             modifier = Modifier.fillMaxWidth(),
-            // colors = CardDefaults.cardColors(containerColor = Color.White)
-            colors = CardDefaults.cardColors(containerColor = Color(0xFFEEEEEE))
-            // elevation = CardDefaults.elevation()
+            colors = CardDefaults.cardColors(containerColor = backgroundColor)
         ) {
             Column(
                 modifier = Modifier.padding(16.dp)
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    request.profilePicture?.let { profilePicture ->
-                        Box(
-                            modifier = Modifier
-                                .size(80.dp)
-                                .clip(CircleShape)
-                                .clickable { /* Handle image click */ }
-                        ) {
-                            Image(
-                                painter = painterResource(id = profilePicture),
-                                contentDescription = null,
-                                modifier = Modifier.fillMaxSize(),
-                                contentScale = ContentScale.Crop
-                            )
-                        }
-                    }
-                    Column(
-                        modifier = Modifier
-                            .padding(start = 16.dp)
-                            .weight(1f)
-                    ) {
-                        Text(
-                            text = request.name,
-                            style = MaterialTheme.typography.labelMedium,
-                            color = Color.Black,
-                            fontSize = 18.sp
-                        )
-                        Text(
-                            text = request.service,
-                            color = Color.Black,
-                            modifier = Modifier.padding(top = 4.dp)
-                        )
-                    }
-                }
+                Text(
+                    text = request.service,
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .align(Alignment.CenterHorizontally), // Aligning text horizontally
+                    style = MaterialTheme.typography.labelMedium,
+                    fontSize = 18.sp
+                )
+
                 Row(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
+                        text = "Client: \n${request.name}",
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+
+                    Text(
                         text = "From price: \n${request.price}${request.currency}",
-                        modifier = Modifier.padding(top = 8.dp),
-                        color = Color.Black
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Location: \n${request.location}",
+                        modifier = Modifier.padding(top = 8.dp)
                     )
 
                     Text(
                         text = "Until: \n${request.date}",
-                        modifier = Modifier.padding(top = 8.dp),
-                        color = Color.Black
-
+                        modifier = Modifier.padding(top = 8.dp)
                     )
                 }
+
+                Spacer(modifier = Modifier.height(14.dp))
 
                 Row(
                     modifier = Modifier.fillMaxWidth(),
@@ -116,23 +107,41 @@ fun RequestItem(request: Request) {
                     Button(
                         onClick = { /* Handle accept */ },
                         colors = ButtonDefaults.buttonColors(
-                            // containerColor = Color.Green
-                            containerColor = Color(0xFF34A239)
-                        ), // Set background color of accept button to green
-                        modifier = Modifier.padding(end = 16.dp) // Add padding to separate buttons
+                            containerColor = Color(0xFF6CC470)
+                        ),
+                        modifier = Modifier.padding(end = 8.dp)
                     ) {
                         Text(text = "Accept")
                     }
+
+
                     Button(
                         onClick = { /* Handle decline */ },
                         colors = ButtonDefaults.buttonColors(
-                            // containerColor = Color.Red
-                            containerColor = Color(0xFFCF1A2F)
-                        ), // Set background color of decline button to red
-                        modifier = Modifier.padding(end = 16.dp) // Add padding to separate buttons
+                            containerColor = Color(0xFFEE7373)
+                        ),
+                        modifier = Modifier.padding(horizontal = 8.dp)
+
                     ) {
                         Text(text = "Decline")
                     }
+
+
+                    ClickableText(
+                        text = AnnotatedString.Builder().apply {
+                            withStyle(style = SpanStyle(textDecoration =
+                            TextDecoration.Underline ,color = Color.Gray)
+                            ) {
+                                append("Detail")
+                            }
+
+                        }.toAnnotatedString(),
+                        onClick = {
+                            // Handle details
+                        },
+                        modifier = Modifier.padding(horizontal = 8.dp)
+                    )
+
                 }
             }
         }
@@ -142,31 +151,26 @@ fun RequestItem(request: Request) {
 
 
 
-val imageResourceIds = listOf(
-    R.drawable._1,
-    R.drawable.adam,
-    R.drawable.gg,
-    // Add more image resource IDs as needed
-)
 
 val requests = listOf(
-    Request("Mohamed", "Apartment Cleaning", 3000, "DZD", "02.02.2024", imageResourceIds[0]),
-    Request("Sarah", "Electrical help", 12000, "DZD", "16.05.2024", imageResourceIds[1]),
-    Request("Ahmed", "Home repairs", 5000, "DZD", "18.05.2024", imageResourceIds[2]),
-    Request("issam", "Painting", 9200, "DZD", "12.05.2024", imageResourceIds[0]),
-    Request("Rosa", "Electrical help", 40100, "DZD", "10.03.2024", imageResourceIds[1]),
-    Request("Ahmed", "Some service", 3000, "DZD", "08.09.2024", imageResourceIds[2]),
-    Request("Mohamed", "Apartment Cleaning", 3000, "DZD", "11.05.2024", imageResourceIds[0])
-    // Add more requests as needed, making sure to match them with corresponding image resource IDs
-)
+    Request("Mohamed", "Apartment Cleaning",
+        3000, "DZD", "02.02.2024", "Zeralda"),
+    Request("Sarah", "Electrical help",
+        12000, "DZD", "16.05.2024","nouvelle ville"),
+    Request("Ahmed", "Home repairs",
+        5000, "DZD", "18.05.2024","blida"),
+    Request("issam", "Painting",
+        9200, "DZD", "12.05.2024","oran"),
 
+    )
 
-@Preview(showBackground = true)
 @Composable
-fun RequestScreen() {
-    Column {
-        requests.forEach { request ->
-            RequestItem(request = request)
-        }
+fun MyRequestsScreen(navController: NavController) {
+    LazyColumn(
+        //   modifier = Modifier.weight(1f)
+    ) {
+        items(requests.size) {
+                request->
+            RequestItem(requests[request])}
     }
 }
