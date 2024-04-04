4ddb5894c18a8bfcd8b29b0f55fa98399bc5ee22
hani <hani11dev@gmail.com>
2024-04-04 15:25:53 +0100

    Jobs backend

--- app/src/main/java/com/example/handyapp/home/jobs/JobsScreen.kt
+++ app/src/main/java/com/example/handyapp/home/jobs/JobsScreen.kt
@@ -1,122 +1,141 @@
-import android.content.res.Configuration
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-@Composable
-fun JobsScreen() {
-    val backgroundColor = if (isSystemInDarkTheme()) Color.DarkGray else Color.White
-    val itemColor = if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray
+data class Job(
+    val category: String,
+    val city: String,
+    val day: String,
+    val description: String,
+    val hour: String,
+    val max: Int,
+    val min: Int,
+    val status: String,
+    val street: String,
+    val title: String,
+    val userId: String,
+    val wilaya: String
+) {
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = backgroundColor // Set background color based on the theme
-    ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            item {
-                TaskItem(
-                    title = "looking for someone to install Shower Drain",
-                    description = "Need someone with good experience on April 13 to install the shower drain in a day.",
-                    budget = "20,500",
-                    bids = 12,
-                    daysLeft = 2,
-                    postedDate = "11-12-2024"
-                )
-            }
+    constructor() : this("", "", "", "", "", 0, 0, "", "", "", "", "")
+}
+class JobsViewModel : ViewModel() {
 
-            item {
-                TaskItem(
-                    title = "I need a professional painter to Paint 3 Rooms",
-                    description = "A professional painter to finish the work of 3 large rooms (7 ft x 10 ft (70 square feet))as...",
-                    budget = "10,600",
-                    bids = 2,
-                    daysLeft = 1,
-                    postedDate = "11-10-2022"
-                )
-            }
-            item {
-                TaskItem(
-                    title = " Repair Kitchen Sink",
-                    description = "Need a plumber to fix my kitchen's sink, it has been leaking water for a while now...",
-                    budget = "20,500",
-                    bids = 12,
-                    daysLeft = 2,
-                    postedDate = "11-11-2021"
-                )
+    private val _jobs = MutableStateFlow<List<Job>>(emptyList())
+    val jobs: StateFlow<List<Job>> = _jobs
+
+    fun getJobs() {
+        viewModelScope.launch {
+            val jobsList = mutableListOf<Job>()
+            val db = Firebase.firestore
+            val jobsCollection = db.collection("Jobs")
+            jobsCollection.get().addOnSuccessListener { result ->
+                for (document in result.documents) {
+                    val job = document.toObject(Job::class.java)!!
+                    jobsList.add(job)
+                }
+                _jobs.value = jobsList
+            }.addOnFailureListener { exception ->
             }
         }
     }
 }
 
-
 @Composable
-fun TaskItem(title: String, description: String, budget: String, bids: Int, daysLeft: Int, postedDate: String) {
-    Surface(
-        modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
-        color = Color.LightGray, // Set background color to light grey
-        shape = RoundedCornerShape(35.dp) // Apply more rounded shape
+fun JobItem(job: Job) {
+    Card(
+        modifier = Modifier
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        elevation =  CardDefaults.cardElevation(defaultElevation = 4.dp)  // Adjust elevation as needed
     ) {
         Column(
             modifier = Modifier.padding(8.dp)
         ) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.headlineSmall,
-                color = if (isSystemInDarkTheme()) Color.DarkGray else Color.Black, // Adjust title color based on theme
-                modifier = Modifier.padding(bottom = 4.dp)
-            )
-            Divider(
-                color = Color.White, // Set white color for the divider
-                thickness = 2.dp, // Adjust thickness as needed
-                modifier = Modifier.padding(bottom = 4.dp)
-            )
-            Text(
-                text = description,
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(bottom = 4.dp),
-                color = if (isSystemInDarkTheme()) Color.DarkGray else Color.Black // Adjust description color based on theme
-            )
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
-                    text = "Budget: $budget DA",
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = if (isSystemInDarkTheme()) Color.Black else Color.Blue // Adjust budget color based on theme
+                    text = job.title,
+                    style = MaterialTheme.typography.headlineMedium, // Use M3 headlineMedium
+                    color = Color.Black,
+                    maxLines = 2, // Allow 2 lines for title
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
-                    text = "Days left: $daysLeft",
-                    style = MaterialTheme.typography.bodySmall,
-                    color = if (isSystemInDarkTheme()) Color.DarkGray else Color.Black // Adjust days left color based on theme
+                    text = job.category,
+                    style = MaterialTheme.typography.bodyMedium, // Use M3 bodyMedium
+                    color = Color.Gray // Adjust category text color as needed
                 )
             }
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = "Bids: $bids",
-                style = MaterialTheme.typography.bodySmall,
-                modifier = Modifier.padding(bottom = 4.dp),
-                color = if (isSystemInDarkTheme()) Color.DarkGray else Color.Black // Adjust bids color based on theme
+                text = job.description,
+                style = MaterialTheme.typography.bodyLarge, // Use M3 bodyLarge
+                color = Color.Gray,
+                maxLines = 3, // Allow 3 lines for description
+                overflow = TextOverflow.Ellipsis
             )
-            Text(
-                text = "Posted: $postedDate",
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(bottom = 8.dp).offset(110.dp), // Adjust offset here
-                color = if (isSystemInDarkTheme()) Color.DarkGray else Color.Black // Adjust posted date color based on theme
-            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Budget: ${job.min} - ${job.max} DA",
+                    style = MaterialTheme.typography.bodyMedium, // Use M3 bodyMedium
+                    color = Color.Blue
+                )
+                IconButton(onClick = { /* Handle favorite button click */ }) {
+                    Icon(Icons.Default.Favorite, contentDescription = "Favorite")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun JobsScreen() {
+    val viewModel = JobsViewModel()
+
+    LaunchedEffect(Unit) {
+        viewModel.getJobs()
+    }
+
+    val jobs by viewModel.jobs.collectAsState(emptyList())
+
+    LazyColumn {
+        items(jobs) { job ->
+            JobItem(job)
         }
     }
 }
